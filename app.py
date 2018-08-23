29ce2dd21f9d65f630f6b4a504f1a6f1c2cfc9aa
voidiker66 <asi327@utexas.edu>
2018-08-22 20:56:19 -0700

    attempt at joining tables in dashboard failing

--- app.py
+++ app.py
@@ -11,10 +11,15 @@ import datetime
 from sqlalchemy import create_engine
 #from wtforms.validators import Required
 from werkzeug.utils import secure_filename
+import os
+import uuid
 
-UPLOAD_FOLDER = '/database/images'
+UPLOAD_FOLDER = '/static/images'
 # only allow images to be uploaded
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 app = Flask(__name__)
 db = SQLAlchemy(app)
@@ -63,30 +68,34 @@ class Animal(db.Model):
 	__tablename__ = 'animal'
 	id = db.Column(db.Integer, primary_key = True)
 	name = db.Column(db.String(32))
-	owner = db.Column(db.Integer)
-	breed = db.Column(db.Integer)
+	owner = db.Column(db.Integer, db.ForeignKey("user.id"))
+	breed = db.Column(db.Integer, db.ForeignKey("breed.id"))
 	picture = db.Column(db.String(32))
+
+	ownerR = db.relationship('User', foreign_keys=[owner])
+	breedR = db.relationship('Breed', foreign_keys=[breed])
 
 	def __init__(self, name, owner, breed, picture):
 		self.name = name
 		self.owner = owner
-		self.email = email
 		self.breed = breed
 		self.picture = picture
 
 class Breed(db.Model):
 	__tablename__ = 'breed'
 	id = db.Column(db.Integer, primary_key = True)
-	name = db.Column(db.String(32))
+	bName = db.Column(db.String(32))
 
 	def __init__(self, name):
-		self.name = name
+		self.bName = name
 
 class Genes(db.Model):
 	__tablename__ = 'genes'
 	id = db.Column(db.Integer, primary_key = True)
 	name = db.Column(db.String(32))
-	breed = db.Column(db.Integer)
+	breed = db.Column(db.Integer, db.ForeignKey("breed.id"))
+
+	breedR = db.relationship('Breed', foreign_keys=[breed])
 
 	def __init__(self, name, breed):
 		self.name = name
@@ -97,12 +106,10 @@ class Attributes(db.Model):
 	id = db.Column(db.Integer, primary_key = True)
 	animal = db.Column(db.Integer)
 	gene = db.Column(db.Integer)
-	dominance = db.Column(db.Integer)
 
-	def __init__(self, animal, gene, dominance):
+	def __init__(self, animal, gene):
 		self.animal = animal
 		self.gene = gene
-		self.dominance = dominance
 
 class LoginForm(Form):
 	username = TextField('Username', [validators.Required()])
@@ -114,10 +121,6 @@ class LoginForm(Form):
 		self.user = None
 
 	def validate(self):
-		rv = Form.validate(self)
-		if not rv:
-			return False
-
 		user = User.query.filter_by(
 			username=self.username.data).first()
 		if user is None:
@@ -133,83 +136,57 @@ class LoginForm(Form):
 		return True
 
 class RegisterForm(Form):
-	username = TextField('Username', [validators.Required()])
-	email = TextField('E-Mail', [validators.Required()])
-	password = PasswordField('Password', [validators.Required()])
-	passwordCopy = PasswordField('Re-Enter Password', [validators.Required()])
-	firstname = TextField('First Name', [validators.Required()])
-	lastname = TextField('Last Name', [validators.Required()])
+	username = TextField('Username', validators=[validators.Required()])
+	email = TextField('E-Mail', validators=[validators.Required(), validators.Email()])
+	password = PasswordField('New Password', [
+		validators.DataRequired(),
+		validators.EqualTo('confirm', message='Passwords must match')
+	])
+	confirm = PasswordField('Repeat Password')
+	firstname = TextField('First Name', validators=[validators.Required(), validators.Length(min=8, max=32, message="Password must be between 8 and 32 characters long")])
+	lastname = TextField('Last Name', validators=[validators.Required()])
 	submit = SubmitField('Submit')
 
 	def __init__(self, *args, **kwargs):
 		Form.__init__(self, *args, **kwargs)
-		self.user = None
 
-	def validate_and_insert(self):
-		rv = Form.validate(self)
-		if not rv:
-			return False
-		if self.username.data and self.password.data and self.passwordCopy.data:
-			if self.password.data == self.passwordCopy.data:
-				user = User(self.username.data, self.firstname.data, self.lastname.data, self.email.data, self.password.data)
-				self.insert(user.username, user.firstname, user.lastname, user.email, user.pw_hash)
-				return True
-			return False
+	def validate(self):
+		if self.username.data and self.password.data and self.confirm.data:
+			if User.query.filter_by(username=self.username.data).first():
+				flash('An account with that username already exists.', category='danger')
+				return False
+			if User.query.filter_by(email=self.email.data).first():
+				flash('An account with that email already exists.', category='danger')
+				return False
+			return True
 		return False
-
-	def insert(self, username, firstname, lastname, email, password):
-		id = e.execute("""insert into user (username, firstname, lastname, password, email, is_active) values (:Username, :FirstName, :LastName, :Password, :Email, '1');""",
-			Username=username, FirstName=firstname, LastName=lastname, Email=email, Password=password
-			)
-		return id
 
 class AddNewForm(Form):
 	name = TextField('Name', [validators.Required()])
 	breed = SelectField('Breed', validators=[validators.Required()], id='select_breed')
 	genes = SelectField('Genes', validators=[validators.Required()], id='select_genes')
-	picture = FileField('Image', [validators.Required()])
+	picture = FileField('Image', validators=[validators.Required()])
 	submit = SubmitField('Submit')
 
 	def __init__(self, *args, **kwargs):
 		Form.__init__(self, *args, **kwargs)
-		self.animal = None
 
-	def validate_and_insert(self):
-		rv = Form.validate(self)
-		if not rv:
-			return False
+	def validate(self):
 		if self.name.data and self.breed.data and self.genes.data and self.picture.data:
-			p = self.upload_file()
-			if p:
-				self.insert(self.name.data, self.breed.data, p, self.genes.data)
+			print(request.files)
+			if 'picture' not in request.files:
+				flash("No file part")
+				return False
+			self.pictureFile = request.files['picture']
+			# if user does not select file, browser also
+			# submit a empty part without filename
+			if self.pictureFile.filename == '':
+				flash('No selected file')
+			elif self.pictureFile and allowed_file(self.pictureFile.filename):
 				return True
+			else:
+				flash('Image must be in png, jpg, jpeg, or gif.')
 		return False
-
-	def insert(self, name, breed, picture, genes):
-		idanimal = e.execute("""insert into animal (name, owner, breed, picture) values (:Name, :Owner, :Breed, :Picture);""",
-			Name=name, Owner=current_user.get_id(), Breed=breed, Picture=picture
-			)
-		idgenes = list()
-		for g in genes:
-			idgenes += e.execute("""insert into attributes (animal, gene, dominance) values (:Animal, :Gene, :Dominance)""",
-				Animal=idanimal, Gene=g, Dominance=2
-			)
-		return (idanimal, idgenes)
-
-	def upload_file():
-		if 'file' not in request.files:
-			flash('No file part')
-			return None
-		file = request.files['file']
-		# if user does not select file, browser also
-		# submit a empty part without filename
-		if file.filename == '':
-			flash('No selected file')
-			return None
-		if file and allowed_file(file.filename):
-			filename = secure_filename(file.filename)
-			file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-			return filename
 
 @app.route('/_get_genes/')
 def _get_genes():
@@ -226,10 +203,7 @@ def home():
 def dashboard():
 	#print(request.cookies.get('evo_lution_session'))
 	user_id = current_user.get_id()
-	animals = Animal.query.filter_by(owner=user_id).all()
-	print(animals)
-	for a in animals:
-		print(a.name)
+	animals = Animal.query.join(Breed).filter_by(owner=user_id).all()
 	return render_template('index.html', data=animals)
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -240,18 +214,21 @@ def login():
 			flash("You're now logged in!", category='success')
 			return redirect('/dashboard')
 		else:
-			flash("No user with that email/password combo", category='failure')
+			flash("No user with that email/password combo", category='danger')
 	return render_template('login.html', form=form)
 
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
 	form = RegisterForm()
 	if form.validate_on_submit():
-		if form.validate_and_insert():
+		if form.validate():
+			user = User(form.username.data, form.firstname.data, form.lastname.data, form.email.data, form.password.data)
+			db.session.add(user)
+			db.session.commit()
 			flash("You're now registered!", category='success')
 			return redirect('/login')
 		else:
-			flash("Error: Check your inputs", category='failure')
+			flash("Error: Check your inputs", category='danger')
 	return render_template('register.html', form=form)
 
 
@@ -268,18 +245,32 @@ def addition():
 	form = AddNewForm()
 	form.breed.choices = [(g.id, g.name) for g in Breed.query.all()]
 	form.genes.choices = [(g.id, g.name) for g in Genes.query.all()]
-	print("here we are")
+
 	if form.validate_on_submit():
 		print("validated on submit")
-		if form.validate_and_insert():
-			print("new animal")
+		if form.validate():
+			file = form.picture.data
+			uuidname = str(uuid.uuid1()) + secure_filename(file.filename)
+			filename = (os.path.join(app.config['UPLOAD_FOLDER'], uuidname))
+			file.save('.' + filename)
+
+			breed = dict(form.breed.choices).get(int(form.breed.data))
+			genes = dict(form.genes.choices).get(int(form.genes.data))
+			
+			animal = Animal(form.name.data, current_user.get_id(), breed, filename)
+			db.session.add(animal)
+			db.session.flush()
+			# for g in form.genes.data:
+			# 	gene = Genes(animal.id, g.value, 2)
+			# 	db.session.add(gene)
+			gene = Attributes(animal.id, Genes.query.filter_by(name=genes).first().id)
+			db.session.add(gene)
+			db.session.commit()
 			flash("New Animal Added!", category='success')
-			return redirect('/addition')
+			return redirect('/dashboard')
 		else:
-			flash("bad input")
-			flash("Error: Check your inputs", category='failure')
-	else:
-		flash("Error: Check your inputs", category='failure')
+			print("here we are")
+			flash("Error: Check your inputs", category='danger')
 	return render_template('addition.html', form=form)
 
 @login_manager.unauthorized_handler
